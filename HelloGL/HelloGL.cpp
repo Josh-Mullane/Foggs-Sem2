38dd3a6de107cfafb962825a996ede12566d1312
Joshua Mullane <114984722+Josh-Mullane@users.noreply.github.com>
2023-02-23 12:45:05 +0000

    Finished week 1

--- HelloGL/HelloGL.cpp
+++ HelloGL/HelloGL.cpp
@@ -5,14 +5,23 @@ HelloGL::HelloGL(int argc, char* argv[])
 	GLUTCallbacks::Init(this);
 
 	glutInit(&argc, argv);
+	glutInitWindowSize(800, 800);
 	glutCreateWindow("Simple OpenGL Program");
-	glutDisplayFunc(Display);
+	glutDisplayFunc(GLUTCallbacks::Display);
 	glutMainLoop();
 }
 
 void HelloGL::Display()
 {
-
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+		glColor4f(1.0f, 0.0f, 1.0f, 0.0f);
+		glVertex2f(-0.75, 0.5);
+		glVertex2f(0.75, 0.5);
+		glVertex2f(0.75, -0.5);
+		glVertex2f(-0.75, -0.5);
+		glEnd();
+	glFlush();
 }
 
 HelloGL::~HelloGL(void)
--- HelloGL/HelloGL.h
+++ HelloGL/HelloGL.h
@@ -5,12 +5,16 @@
 #include "GL\freeglut.h"
 #include "GLUTCallbacks.h"
 
+
 class HelloGL
 {
 public:
 	HelloGL(int argc, char* argv[]);
 
 	~HelloGL(void);
+
+	void Display();
+
+	void DrawPolygon();
 };
 
-void Display();
